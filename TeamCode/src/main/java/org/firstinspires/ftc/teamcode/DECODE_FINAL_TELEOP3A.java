9a6757e9f327f4dc90cc331dd4107f4c9091b252
Vishwesh <vishweshck3@gmail.com>
2026-02-18 21:41:39 -0800

    Final teleop 2/18

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DECODE_FINAL_TELEOP3A.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DECODE_FINAL_TELEOP3A.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -31,23 +32,12 @@ public class DECODE_FINAL_TELEOP3A extends LinearOpMode {
 ==========================================*/
 
 
-// CAMERA CODE INITIALIZATION FOR APRIL TAG and TURRET SERV0 ALIGNMENT IS BELOW
 
     private static final double CAMERA_HEIGHT_INCHES = 12.9;   // camera lens height
     private static final double TAG_HEIGHT_INCHES    = 29.5;   // AprilTag center height
     private static final double CAMERA_PITCH_DEGREES = 5.0;    // camera upward tilt
-    private static final double SERVO_CENTER = 0.9;
 
-    // Servo position that points the mechanism straight ahead (aligned with robot center)
-    private static final double SERVO_MIN = 0.8;
 
-    // Lowest safe servo position to prevent hitting the robot or hard stops
-    private static final double SERVO_MAX = 1;
-
-    // Highest safe servo position to prevent hitting the robot or hard stops
-    private static final double SERVO_GAIN = 0.005;
-
-    // How much the servo moves per degree of horizontal error (tx); higher = faster movement
     private static final double TX_DEADBAND = 4.0;
     private AnalogInput turretinput;
     double MAXV = 0.47,MINV = 2.79;
@@ -55,18 +45,21 @@ public class DECODE_FINAL_TELEOP3A extends LinearOpMode {
     double distanceInches = 0;
 
     //================CONSTANTS================//
+    public static final double TURRET_RIGHT = 0.88;
+    public static final double TURRET_LEFT = 0.68;
+    public static final double TURRET_CENTER = 0.778;
     public static final double KICKER_DOWN = 0.9;//0.225
     public static final double KICKER_UP = 0.53;//0.6
-    public static final double ARM_SERVO_POSITION = 0.245;
+    public static final double ARM_SERVO_POSITION = 0.042;
     public static final double INTAKE_IDLE = -0.1;
-    public static final double BOOTKICKER_IDLE = -0.1;
+    public static final double BOOTKICKER_IDLE = -0.1;//-0.1
     public static final double INTAKE_COLLECT = -0.9;
-    public static final double BOOTKICKER_COLLECT = -0.4;
+    public static final double BOOTKICKER_COLLECT = -0.4;//-0.4
     public static final double INTAKE_ABORT = 0.5;
     public static final double BOOTKICKER_ABORT = 0.5;
-    public static final double ARM_ABORT = 0.3;
+    public static final double ARM_ABORT = 0.05;
     public static final double INTAKE_SHOOT = -0.2;
-    public static final double BOOTKICKER_SHOOT = -0.2;
+    public static final double BOOTKICKER_SHOOT = -0.8;
     public static final double MAX_COLOR_SENSED_DISTANCE = 7;
 
 
@@ -83,11 +76,13 @@ public class DECODE_FINAL_TELEOP3A extends LinearOpMode {
     private RevColorSensorV3 distanceSensor;
     private PinpointLocalizer pinpointLocalizer;
 
-    Range range1 = new Range(0, 55, 1100);
-    Range range2 = new Range(55, 80, 1200);
+    Range range1 = new Range(0, 55, 1630);//1100
+    Range range2 = new Range(55, 73, 1800);//1200
+    //Range range3 = new Range(73,85,)
 
-    Range range4 = new Range(112, Integer.MAX_VALUE, 1430);
-    Range range3 = new Range(80, 112, 1410);
+    Range range3 = new Range(112, Integer.MAX_VALUE, 2140);//1430
+    Range range4 = new Range(73, 112, 2115);//1410
+
 
     //STATE MACHINE SETUP
     enum RobotState {
@@ -108,16 +103,17 @@ public class DECODE_FINAL_TELEOP3A extends LinearOpMode {
     boolean lastB = false;
 
     // Latch for auto-fire
-    boolean firingEnabled = false;
     boolean lastOptions = false;
     private Servo turret = null;
     public Servo armservo = null;
-    double tx1=0;
+    double tx=0;
     double ty=0;
     int current_kicker_stage = 0;
+    boolean kickerPartial = false;
     double newPos;
     boolean gamepad1LastA = false;
     boolean aPressed = false;
+    int ballsShot = 0;
 
     @Override
     public void runOpMode() {
@@ -138,6 +134,23 @@ public class DECODE_FINAL_TELEOP3A extends LinearOpMode {
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         turretinput = hardwareMap.get(AnalogInput.class,"turretinput");
 
+        //PID Coefficients
+        PIDFCoefficients shooterCoefficients = shooter.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER);
+        double p = shooterCoefficients.p;
+        double i = shooterCoefficients.i;
+        double d = shooterCoefficients.d;
+        double f = shooterCoefficients.f;
+        PIDFCoefficients pidfNew = new PIDFCoefficients(14, i, d, f);
+        shooter.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pidfNew);
+
+        telemetry.addData("p: ",p);//10
+        telemetry.addData("i: ",i);//3
+        telemetry.addData("d: ",d);//0
+        telemetry.addData("f: ",f);//0
+        telemetry.update();
+
+
+
 
         limelight.pipelineSwitch(0); // AprilTag pipeline
         limelight.start();
@@ -148,7 +161,7 @@ public class DECODE_FINAL_TELEOP3A extends LinearOpMode {
 
         kicker.setPosition(KICKER_DOWN);
         armservo.setPosition(ARM_SERVO_POSITION);
-        turret.setPosition(0.778);
+        turret.setPosition(TURRET_CENTER);
 
         shooter.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -183,8 +196,6 @@ public class DECODE_FINAL_TELEOP3A extends LinearOpMode {
             rightFront.setPower((rotY - rotX - rx) / denom);
             rightBack.setPower((rotY + rotX - rx) / denom);
 
-            //distance mesuring
-
             // APRIL TAG DETECTION
             double curV = turretinput.getVoltage();
             double turrpos = (curV-MINV) / (MAXV-MINV);
@@ -194,161 +205,122 @@ public class DECODE_FINAL_TELEOP3A extends LinearOpMode {
                 if(aPressed && !gamepad1LastA) {
                     gamepad1.rumble(100);
                 }
-
                 ty = result.getTy();
-
-
                 double totalVerticalAngle = CAMERA_PITCH_DEGREES + ty;
-
                 // Update the existing distanceInches variable (no 'double' prefix)
                 distanceInches = (TAG_HEIGHT_INCHES - CAMERA_HEIGHT_INCHES) /
                         Math.tan(Math.toRadians(totalVerticalAngle));
-
                 //telemetry.addData("Tag Seen", true);
             }
             gamepad1LastA = aPressed;
             // A(Toggle for IDLE and COLLECT) B(Toggle for SHOOT and IDLE)
-
             boolean a = gamepad2.a;
             boolean b = gamepad2.b;
             boolean xPressed = gamepad2.x;
 
-            if (result != null && result.isValid() && result.getTx() != tx1) {
+            if (result != null && result.isValid() && result.getTx() != tx) {
                 int detectedID = result.getFiducialResults().get(0).getFiducialId();
                 telemetry.addData("sees april tag, this is detectedid: ", detectedID);
                 //telemetry.update();
-                tx1 = result.getTx();
+                tx = result.getTx();
                 if (detectedID == 20) {
-                    double val = Math.min(Math.max(0.68, turrpos + (tx1 / 280)-0.005), 0.88);
+                    double val = Math.min(Math.max(TURRET_LEFT, turrpos + (tx / 360)-0.005), TURRET_RIGHT);
                     turret.setPosition(val);
                     //sleep(900);
                     telemetry.addData("servo target pos blue", val );
-                    //telemetry.update();
                 } else if (detectedID == 24) {
-                    double val = Math.min(Math.max(0.68, turrpos + (tx1 / 280)+0.005), 0.88);
+                    double val = Math.min(Math.max(TURRET_LEFT, turrpos + (tx / 360)+0.005), TURRET_RIGHT);
                     turret.setPosition(val);
                     //sleep(900);
                     telemetry.addData("servo target pos red", val );
-                    //telemetry.update();
                 }
-
-
-//                        double val = Math.min(Math.max(0.8, turret.getPosition() + (tx / 360)), 1);
-//                        turret.setPosition(val);
-                //  sleep(900);
-                //  telemetry.addData("servo target pos", val);
-                //telemetry.update();
             }
 
             if (a && !lastA) {
                 if (currentState == RobotState.COLLECT) {
                     currentState = RobotState.IDLE;
-                    // tx = 0;
+                    ballsShot = 0;
                 } else {
                     currentState = RobotState.COLLECT;
-                    //  tx = 0;
+                    ballsShot = 0;
                 }
             }
             if (b && !lastB) {
                 if (currentState == RobotState.SHOOT) {
-
+                    ballsShot = 0;
                     currentState = RobotState.IDLE;
-                    //   tx = 0;
                 } else {
                     currentState = RobotState.SHOOT;
-                    //   gamepad1.rumble(100);
-//                    if (result != null && result.isValid()) {
-//                        int detectedID = result.getFiducialResults().get(0).getFiducialId();
-//
-//                        if (detectedID == 20) {
-//                            double val = Math.min(Math.max(0.68, turrpos + (tx / 360)), 0.88);
-//                            //turret.setPosition(val - 0.005);
-//                            sleep(900);
-//                            telemetry.addData("servo target pos blue", val );
-//                            telemetry.update();
-//                        } else if (detectedID == 24) {
-//                            double val = Math.min(Math.max(0.68, turrpos + (tx / 360)), 0.88);
-//                            //turret.setPosition(val + 0.005);
-//                            sleep(900);
-//                            telemetry.addData("servo target pos red", val );
-//                            telemetry.update();
-//                        }
-//
-//
-////                        double val = Math.min(Math.max(0.8, turret.getPosition() + (tx / 360)), 1);
-////                        turret.setPosition(val);
-//                        //  sleep(900);
-//                        //  telemetry.addData("servo target pos", val);
-//                        telemetry.update();
-//                    }
+
 
                 }
             }
             if(xPressed) {
                 currentState = RobotState.ABORT;
             }
+//            if (gamepad2.right_bumper){
+//                turret.setPosition(TURRET_CENTER);
+//            }
             lastA = a;
             lastB = b;
 
 
             // STATE MACHINE
             switch (currentState) {
-
                 case IDLE:
-
                     shooter.setVelocity(shooterVelocity(distanceInches));
                     intake.setPower(INTAKE_IDLE);
-                    //turret.setPosition(0.78);
                     bootkicker.setPower(BOOTKICKER_IDLE);
-                    kicker.setPosition(KICKER_DOWN);
-                    armservo.setPosition(0.24);
+                    armservo.setPosition(ARM_SERVO_POSITION);
                     break;
-
                 case COLLECT:
+                    if (distanceCM<7){
+                        kicker.setPosition(0.7);
+                        kickerPartial = true;
+                    }
                     shooter.setVelocity(shooterVelocity(distanceInches));
-                    kicker.setPosition(KICKER_DOWN);
                     bootkicker.setPower(BOOTKICKER_COLLECT);
                     intake.setPower(INTAKE_COLLECT);
-                    //turret.setPosition(0.78);
-                    armservo.setPosition(0.24);
+                    armservo.setPosition(ARM_SERVO_POSITION);
                     break;
-
                 case SHOOT:
+                    if(ballsShot == 0) {
+                        intake.setPower(0);
+                        bootkicker.setPower(0);
+                    }
+                    else {
+                        intake.setPower(INTAKE_COLLECT);
+                        bootkicker.setPower(BOOTKICKER_COLLECT);
+                    }
                     shooter.setVelocity(shooterVelocity(distanceInches));
-
-                    if (distanceCM < 7 && kickerStage == 0) {
+                    if ((distanceCM < 7 && kickerStage == 0) || kickerPartial) {
                         runtime.reset();
                         kickerStage = 1;
-                    }
+                        kickerPartial = false;
 
+                    }
 // delay 100ms
                     if (kickerStage == 1 && runtime.milliseconds() >= 100 ) {
                         kicker.setPosition(KICKER_UP);
                         runtime.reset();
                         kickerStage = 2;
                     }
-
 // delay 500ms
                     if (kickerStage == 2 && runtime.milliseconds() >= 500) {
                         kicker.setPosition(KICKER_DOWN);
                         runtime.reset();
                         kickerStage = 3;
                     }
-
 // delay 200ms
                     if (kickerStage == 3 && runtime.milliseconds() >= 200) {
                         telemetry.addData("kicker shoot", true);
+                        ballsShot++;
                         kickerStage = 0;
                     }
-
                     // stop when no ball
                     if (distanceCM >= 7) {
-                        kicker.setPosition(KICKER_DOWN);
-                        armservo.setPosition(0.25);
+                        armservo.setPosition(ARM_SERVO_POSITION+0.005);
                     }
-
-                    intake.setPower(INTAKE_COLLECT);
-                    bootkicker.setPower(BOOTKICKER_COLLECT);
                     telemetry.addData("shoot mode", true);
                     //telemetry.update();
                     break;
@@ -357,7 +329,6 @@ public class DECODE_FINAL_TELEOP3A extends LinearOpMode {
                     bootkicker.setPower(BOOTKICKER_ABORT);
                     armservo.setPosition(ARM_ABORT);
                     kicker.setPosition(KICKER_DOWN);
-                    //turret.setPosition(0.78);
             }
 
             //telemetry
@@ -374,6 +345,7 @@ public class DECODE_FINAL_TELEOP3A extends LinearOpMode {
             telemetry.addData("ty (deg)", ty);
             telemetry.addData("servo current pos", turret.getPosition());
             telemetry.addData("turret exact pose", turrpos);
+            telemetry.addData("kicker pos",kicker.getPosition());
             dashboardTelemetry.addData("Boot Kicker Power",bootkicker.getCurrent(CurrentUnit.AMPS));
             dashboardTelemetry.addData("Intake Power",intake.getCurrent(CurrentUnit.AMPS));
             dashboardTelemetry.addData("LeftFront Power",leftFront.getCurrent(CurrentUnit.AMPS));
@@ -385,6 +357,7 @@ public class DECODE_FINAL_TELEOP3A extends LinearOpMode {
             telemetry.update();
         }
     }
+
     public double shooterVelocity(double distanceInInches) {
         if (distanceInInches>=range1.l && distanceInInches<range1.r){
             telemetry.addData("range1", range1.speed);
